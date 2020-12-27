5763437d101b44df877f7580e5567fd235d00a29
kaznak <kaznak.at.work@gmail.com>
2020-12-27 23:06:05 +0900

    restructure postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,10 @@ const production = process.env.NODE_ENV == 'production'
 
 module.exports = {
   plugins: [
-    require('tailwindcss')({}),
-    require('autoprefixer')({}),
+    require('postcss-import'),
+    require('lost'),
+    require('tailwindcss'),
+    require('postcss-preset-env')({ stage: 1 }),
     production &&
       require('cssnano')({
         preset: [
